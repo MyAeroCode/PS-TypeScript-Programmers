572a9cf370750d72b38175189e11a91cf5540fc8
MyAeroCode <meteo_meteo@naver.com>
2020-08-22 19:22:06 +0900

    add: 카펫

--- playground/src/solution.ts
+++ playground/src/solution.ts
@@ -1,9 +1,9 @@
-export function solution() {
-    let answer = "Hello, World!";
-    return answer;
+export function solution(brown: number, yellow: number): number[] {
+    for (let i = 0; i <= Math.sqrt(yellow); i++) {
+        if (yellow % i !== 0) continue;
+        const w = yellow / i + 2;
+        const h = i + 2;
+        if (w * h === brown + yellow) return [w, h];
+    }
+    throw new Error();
 }
-
-// function main() {
-//     console.log(solution());
-// }
-// main();
